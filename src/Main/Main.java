c8a28051ee1218f1e245d00251a391c0eee6e672
CyboBrown <seville2020christian@gmail.com>
2023-05-09 22:55:31 +0800

    connected database to main code

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,12 +5,30 @@ import QuizQuestions.MultipleChoice.Choice;
 import QuizQuestions.Quiz;
 
 import javax.swing.*;
+import java.io.IOException;
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
         Quiz myquiz = new Quiz("Pabs");
         myquiz.addMultipleChoiceQuestion("Who is a?", 4, Choice.A)
                 .addChoice("R").addChoice("B").addChoice("C").addChoice("D");
-        myquiz.getQuestionNumber(1).checkAnswer(Choice.A);
+        System.out.println(myquiz.getQuestionNumber(1).checkAnswer(Choice.A));
+        myquiz.log();
+        System.out.println("\n");
+        SQLConnector sqlc;
+        try {
+            sqlc = new SQLConnector();
+            sqlc.postQuiz(myquiz);
+            Quiz newquiz = sqlc.getQuiz(1);
+            System.out.println(newquiz.getQuestionNumber(1).checkAnswer(Choice.A));
+            newquiz.log();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
