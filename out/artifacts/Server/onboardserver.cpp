c8456d95e67d9861fc9d5f96fb34990fb69e12c3
Xue64 <rufelleemmanuelpactol7@gmail.com>
2023-05-15 21:19:15 +0800

    update properties

---
+++ out/artifacts/Server/onboardserver.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int main (int argc, char ** argv){
+    std::string s = "cd C:/Users/Rufelle/IdeaProjects/OffineQuizNetworking/out/artifacts/Server && java -jar OffineQuizNetworking.jar";
+    // std::string arg = "cd ";
+    // arg.append(argv[0]);
+    // arg.append(" && ");
+    // s = arg + s;
+    system(s.c_str());
+}
+
+/* =================================
+
+This comment block is made for the 
+red-black tree.
+
+
+The red-black tree is made out of
+consecutively-asynchronous variables...
+
+===================================*/
